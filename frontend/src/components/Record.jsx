d71dae7617bc0b3a71358f01c8a6b4953ce01bb2
Bhavil-13 <bhavilsharma83@gmail.com>
2024-12-08 12:37:20 +0530

    starting to use kubernetes

--- frontend/src/components/Record.jsx
+++ frontend/src/components/Record.jsx
@@ -1,6 +1,10 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
+// const API_URL = process.env.REACT_APP_API_URL;
+// console.log("HIIIIIIIIIIIIIIIIIIIIIIII")
+// console.log(API_URL)
+
 export default function Record() {
   const [form, setForm] = useState({
     name: "",
